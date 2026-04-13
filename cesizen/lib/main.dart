c344c5ef72ec711258f410137400f36ce1508e6d
titouvgx <titouan.vigouroux29@gmail.com>
2026-04-13 10:53:38 +0200

    Undo Project

--- cesizen/lib/main.dart
+++ cesizen/lib/main.dart
@@ -1,19 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'auth_service.dart';
 import 'home_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  await dotenv.load(fileName: '.env');
-
   await Supabase.initialize(
-    url: dotenv.env['SUPABASE_URL']!,
-    anonKey: dotenv.env['SUPABASE_ANON_KEY']!,
-  );
+  url: 'https://kfelnflvpsymrkdredpo.supabase.co',
+  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtmZWxuZmx2cHN5bXJrZHJlZHBvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM4MzY3NTksImV4cCI6MjA4OTQxMjc1OX0.-GANEZDBRpQ_MI0IYSlcYRE0Z2eDBU91q59ECitIY6U',
+);
 
+  // Restaure la session si l'utilisateur était déjà connecté
   await AuthService.restaurerSession();
 
   runApp(const CESIZenApp());
